3e66253f2f32aebeee88ab1e41e754820c0fed40
Ernesto Vargas <elhibrido@gmail.com>
2012-02-23 10:33:57 -0600

    Better looking message board

--- Messagee/MessageViewController.m
+++ Messagee/MessageViewController.m
@@ -130,7 +130,7 @@ NSTimer *timer;
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
 	CGSize size = [[[_statuses objectAtIndex:indexPath.row] content] sizeWithFont:[UIFont systemFontOfSize:14] constrainedToSize:CGSizeMake(300, 9000)];
-	return size.height + 40;
+	return size.height + 60;
 }
 
 #pragma mark UITableViewDataSource methods
@@ -142,29 +142,74 @@ NSTimer *timer;
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 	NSString* reuseIdentifier = @"Cell Identifier";
 	UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
+    
+    UIImageView *balloonView;
+	UILabel *contentLabel;
+    UILabel *usernameLabel;
+    
 	if (nil == cell) {
 		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
-		cell.textLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
-        cell.textLabel.textColor = [UIColor colorWithRed:0.392 green:0.682 blue:0.847 alpha:1];
-        cell.textLabel.backgroundColor = [UIColor whiteColor];
-		cell.textLabel.numberOfLines = 0;
-		//cell.textLabel.backgroundColor = [UIColor clearColor];
-        cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica" size:12];
-        cell.detailTextLabel.textColor = [UIColor whiteColor];
+
+        tableView.separatorStyle = UITableViewCellSeparatorStyleNone;		
+		
+		balloonView = [[UIImageView alloc] initWithFrame:CGRectZero];
+		balloonView.tag = 1;
+        
+        usernameLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        usernameLabel.backgroundColor = [UIColor clearColor];
+		usernameLabel.textColor = [UIColor colorWithRed:0.392 green:0.682 blue:0.847 alpha:1];
+        usernameLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
+        usernameLabel.tag = 3;
+        
+		contentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		contentLabel.backgroundColor = [UIColor clearColor];
+		contentLabel.tag = 2;
+		contentLabel.numberOfLines = 0;
+		contentLabel.lineBreakMode = UILineBreakModeWordWrap;
+		contentLabel.font = [UIFont systemFontOfSize:14.0];
+        
+        
+		UIView *message = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, cell.frame.size.width, cell.frame.size.height)];
+		message.tag = 0;
+		[message addSubview:balloonView];
+        [message addSubview:usernameLabel];
+		[message addSubview:contentLabel];
+		[cell.contentView addSubview:message];
+		
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"messageArrow"]];
+        imageView.center = CGPointMake(65, 30);
+        [cell.contentView addSubview:imageView];
+        
+        NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString:[NSString stringWithFormat:@"%@", [[[_statuses objectAtIndex:indexPath.row] actor] picture] ]]];
+        UIImage *imagee = [UIImage imageWithData:imageData];
+        cell.imageView.image = [imagee imageScaledToSize:CGSizeMake(42, 42)];
+        
+        cell.imageView.layer.masksToBounds = YES;
+        cell.imageView.layer.cornerRadius = 22;
+        cell.imageView.layer.borderColor = [[UIColor colorWithRed:0.169 green:0.169 blue:0.169 alpha:1] CGColor];
+        cell.imageView.layer.borderWidth = 2;
 	}
-    NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString:[NSString stringWithFormat:@"%@", [[[_statuses objectAtIndex:indexPath.row] actor] picture] ]]];
-    UIImage *image = [UIImage imageWithData:imageData];
-    cell.imageView.image = [image imageScaledToSize:CGSizeMake(42, 42)];
+	else
+	{
+		balloonView = (UIImageView *)[[cell.contentView viewWithTag:0] viewWithTag:1];
+        usernameLabel = (UILabel *)[[cell.contentView viewWithTag:0] viewWithTag:3];
+		contentLabel = (UILabel *)[[cell.contentView viewWithTag:0] viewWithTag:2];
+	}
+	
+	NSString *text = [[_statuses objectAtIndex:indexPath.row] content];
+	CGSize size = [text sizeWithFont:[UIFont fontWithName:@"Helvetica" size:12] constrainedToSize:CGSizeMake(230.0f, 480.0f) lineBreakMode:UILineBreakModeWordWrap];
+	
+	UIImage *balloon;
+    balloonView.frame = CGRectMake(65, 15, 230, size.height + 55);
+    balloon = [[UIImage imageNamed:@"ballon.png"] stretchableImageWithLeftCapWidth:24 topCapHeight:15];
+    contentLabel.frame = CGRectMake(70, 35, 210, size.height + 30);
+    usernameLabel.frame = CGRectMake(70, 25, 230, 10);
 
-    cell.imageView.layer.masksToBounds = YES;
-    cell.imageView.layer.cornerRadius = 22;
-    cell.imageView.layer.borderColor = [[UIColor colorWithRed:0.169 green:0.169 blue:0.169 alpha:1] CGColor];
-    cell.imageView.layer.borderWidth = 4;
-
-    //cell.contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"listbg.png"]];
-    cell.textLabel.text = [[[_statuses objectAtIndex:indexPath.row] actor] displayName];
-    cell.detailTextLabel.text = [[_statuses objectAtIndex:indexPath.row] content];
+    balloonView.image = balloon;
+    usernameLabel.text = @"Hola";
+	contentLabel.text = text;
+    
 	return cell;
 }
 
